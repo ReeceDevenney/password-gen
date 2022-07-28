d8bbb7655746beddd752e5b395244e6291313d22
Reece Devenney <reecedevenney@gmail.com>
2022-07-28 19:38:37 -0400

    added validation to password generation

--- assets/script.js
+++ assets/script.js
@@ -4,61 +4,99 @@ var generateBtn = document.querySelector("#generate");
 var masterArray = []
 var lowerCaseArray = [...Array(26)].map((_, i) => String.fromCharCode(i + 97));
 var upperCaseArray = [...Array(26)].map((_, i) => String.fromCharCode(i + 65));
-var numberArray = ['0','1', '2','3','4','5','6','7','8','9']
+var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var specialCharactersArray = ['-', '+', '*', '/', '=', ']', '[', '}', '{', '<', '>', '?', '!', '@', '#', '$', '%', '&', '*']
 // Write password to the #password input
 function writePassword() {
     masterArray = []
-    var generatePassword = function() {
+    var generatePassword = function () {
         // determines password length and what type of characters will be in the password
         var passwordLength = window.prompt('how long will your password be? must be between 8-128 characters')
         if (passwordLength >= 8 && passwordLength <= 128) {
-            console.log(passwordLength)
         } else {
             window.alert('please enter a value between 8-128 characters');
             return;
         }
-        var lowerCase = window.confirm ('add lower case characters to password?');
+        var lowerCase = window.confirm('add lower case characters to password?');
         if (lowerCase) {
             masterArray = masterArray.concat(lowerCaseArray);
-            console.log(masterArray)
         }
-        var upperCase = window.confirm ('add uper case characters to password?');
+        var upperCase = window.confirm('add uper case characters to password?');
         if (upperCase) {
             masterArray = masterArray.concat(upperCaseArray);
-            console.log(masterArray)
         }
-        var number = window.confirm ('add number characters to password?');
+        var number = window.confirm('add number characters to password?');
         if (number) {
             masterArray = masterArray.concat(numberArray);
-            console.log(masterArray)
         }
-        var specialCharacters = window.confirm ('add special Characters characters to password?');
+        var specialCharacters = window.confirm('add special Characters to password?');
         if (specialCharacters) {
             masterArray = masterArray.concat(specialCharactersArray);
-            console.log(masterArray)
         }
         // cancels the function if not character types were approved
-        if (masterArray.length <= 0){
+        if (masterArray.length <= 0) {
             window.alert('Please chose at least one type of character for your password.');
             return;
         }
         // resets the password to blank, then generates a new password 
-        var newPassword = ''
-        for (i = 0; i < passwordLength; i++){
-            var randomNumber = Math.floor(Math.random() * masterArray.length);
-            newPassword = newPassword + masterArray[randomNumber];
-            console.log(newPassword)
-        }
+        var passwordTest = function () {
+            var newPassword = ''
+            for (i = 0; i < passwordLength; i++) {
+                var randomNumber = Math.floor(Math.random() * masterArray.length);
+                newPassword = newPassword + masterArray[randomNumber];
+            }
+            // test if the generated password contains at least 1 of the requested character type
+            if (lowerCase) {
+                i = 0;
+                result = false;
+                do {
+                    result = lowerCaseArray.includes(newPassword[i])
+                    i = i + 1
+                } while (result === false && i < newPassword.length);
+                if (result === false) {
+                    passwordTest()
+                }
+            }
+            if (upperCase) {
+                i = 0;
+                result = false;
+                do {
+                    result = upperCaseArray.includes(newPassword[i])
+                    i = i + 1
+                } while (result === false && i < newPassword.length);
+                if (result === false) {
+                    passwordTest()
+                }
+            }
+            if (number) {
+                i = 0;
+                result = false;
+                do {
+                    result = numberArray.includes(newPassword[i])
+                    i = i + 1
+                } while (result === false && i < newPassword.length);
+                if (result === false) {
+                    passwordTest()
+                }
+            }
+            if (specialCharacters) {
+                i = 0;
+                result = false;
+                do {
+                    result = specialCharactersArray.includes(newPassword[i])
+                    i = i + 1
+                } while (result === false && i < newPassword.length);
+                if (result === false) {
+                    passwordTest()
+                }
+            }
         return newPassword;
+        }
+        return passwordTest();
     }
     var password = generatePassword()
-
     var passwordText = document.querySelector("#password");
-
     passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
